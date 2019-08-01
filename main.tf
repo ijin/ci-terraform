01183bec8b2ce38d52368343434b010377666931
Michael H. Oshita <ijinpublic+github@gmail.com>
2019-08-01 22:40:35 +0900

    ecr

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ locals {
   ssm = "/${var.app}/${terraform.workspace}"
 }
 
-#resource "aws_ecr_repository" "ci-tf" {
-#  name = "ci-tf-${terraform.workspace}"
-#}
+resource "aws_ecr_repository" "ci-tf" {
+  name = "ci-tf-${terraform.workspace}"
+}
 
 #data "aws_ssm_parameter" "db_passord" {
 #  name = "${local.ssm}/db_password"
